94bd5d7f6787031465b7c53015f20b401d4cc24c
bj-white <10495799@qq.com>
2021-01-04 20:27:54 +0800

    by white

--- src/main/java/com/it/bw/controller/RoleController.java
+++ src/main/java/com/it/bw/controller/RoleController.java
@@ -2,21 +2,10 @@ package com.it.bw.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.it.bw.pojo.Role;
 import com.it.bw.pojo.RoleQueryVo;
-import com.it.bw.util.ReturnData;
 
 @Controller
 @RequestMapping("/role")
-public class RoleController extends BaseController<Role, RoleQueryVo> {
-	
-	@RequestMapping("/getByPage")
-	@ResponseBody
-	public ReturnData getByPage(RoleQueryVo q1) {
-		System.out.println(q1.getName());
-		return super.getByPage(q1);
-	}
-	
-}
+public class RoleController extends BaseController<Role, RoleQueryVo> {}
